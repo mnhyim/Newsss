5fb6c471df5fd74d79c2d226b307c724a60c7d13
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2025-09-18 19:24:57 +0800

    feat: add button to open the article in browser

--- app/src/main/java/com/mnhyim/newsss/ui/feature/detail/Detail.kt
+++ app/src/main/java/com/mnhyim/newsss/ui/feature/detail/Detail.kt
@@ -1,15 +1,19 @@
 package com.mnhyim.newsss.ui.feature.detail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mnhyim.newsss.domain.model.News
@@ -33,6 +37,8 @@ private fun DetailScreen(
     news: News,
     modifier: Modifier = Modifier
 ) {
+    val uriHandler = LocalUriHandler.current
+
     Column(
         modifier = modifier.padding(16.dp, 0.dp, 16.dp, 16.dp)
     ) {
@@ -56,5 +62,12 @@ private fun DetailScreen(
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(horizontal = 16.dp)
         )
+        Button(
+            onClick = { uriHandler.openUri(news.url)},
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            Text("Read full article")
+        }
     }
 }
